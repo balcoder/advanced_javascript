34b767141b6373559290e6f037ba34ba69685dac
balcoder <des.barrett@gmail.com>
2026-03-24 07:46:39 +0000

    added destructoring

--- assets/main.js
+++ assets/main.js
@@ -1194,6 +1194,71 @@ function reverseArray() {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+// Nested Destructoring
+///////////////////////////////////////////////////////////////////////////////
+const instructor = ({
+  name: { first, last } = { first: "Matt", last: "Pocock" },
+  isHilarious = false,
+} = {}) => {
+  return [first, last, isHilarious];
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// Destructuring
+///////////////////////////////////////////////////////////////////////////////
+/* 
+Write a function called displayStudentInfo which accepts an object and returns the
+string "Your full name is" concatenated with the value of the first key and a space
+and then the value of the last key. See if you can destructure this object inside of
+the function.
+
+Examples:
+    displayStudentInfo({first: 'Elie', last:'Schoppik'}) // 'Your full name is Elie Schoppik')
+*/
+
+const displayStudentInfo = (obj) => {
+  var { first, last } = obj;
+  return `Your full name is ${first} ${last}`;
+};
+
+/* 
+Write a function called printFullName which accepts an object and returns the string
+"Your full name is" concatenated with the value of the first key and a space and then
+the value of the last key. See if you can destructure this object DIRECTLY from the
+parameters. The output of the printFullName function should be the exact same as the
+displayStudentInfo function. 
+
+Examples:
+    printFullName({first: 'Elie', last:'Schoppik'}) // 'Your full name is Elie Schoppik'
+*/
+
+const printFullName = ({ first, last }) => {
+  return `Your full name is ${first} ${last}`;
+};
+
+/* 
+Write a function called createStudent which accepts as a parameter, a default parameter which is a destructured object with the key of likesES2015 and value of true, and key of likesJavaScript and value of true. 
+
+    If both the values of likesJavaScript and likesES2015 are true, the function should return the string 'The student likes JavaScript and ES2015'. 
+    If the value of likesES2015 is false the function should return the string 'The student likes JavaScript!'
+    If the value of likesJavaScript is false the function should return the string 'The student likesES2015!'
+    If both the value of likesJavaScript and likesES2015 are false, the function should return the string 'The student does not like much...'
+
+Examples:
+    createStudent() // 'The student likes JavaScript and ES2015')
+    createStudent({likesES2015:false}) // 'The student likes JavaScript!')
+    createStudent({likesJavaScript:false}) // 'The student likes ES2015!')
+    createStudent({likesJavaScript:false, likesES2015:false}) // 'The student does not like much...')
+*/
+
+const createStudent = ({ likesES2015 = true, likesJavaScript = true } = {}) => {
+  if (likesES2015 && likesJavaScript)
+    return "The student likes JavaScript and ES2015";
+  if (likesES2015) return "The student likes ES2015";
+  if (likesJavaScript) return "The student likes JavaScript";
+  return "The student does not like much...";
+};
+///////////////////////////////////////////////////////////////////////////////
 // ES2015 Classes
 ///////////////////////////////////////////////////////////////////////////////
 
